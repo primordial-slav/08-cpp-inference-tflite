9da708797fd581a9bd0dd6e00a62ee8a09a34a6a
Boris Papic <boris.papic@protech.rs>
2023-07-28 10:47:08 +0000

    Created dummy img and fixed nans

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <cstdio>
+#include <fstream>
 #include <opencv2/opencv.hpp>
 
 #include "tensorflow/lite/interpreter.h"
 #include "tensorflow/lite/kernels/register.h"
 #include "tensorflow/lite/model.h"
 #include "tensorflow/lite/optional_debug_tools.h"
+#include "decode_boxes.h"
 
 #define TFLITE_MINIMAL_CHECK(x)                              \
   if (!(x)) {                                                \
@@ -13,55 +15,93 @@
   }
 
 int main() {
-  std::unique_ptr<tflite::FlatBufferModel> model =
-    tflite::FlatBufferModel::BuildFromFile("../models/face_detection_full_range.tflite");
+    std::unique_ptr<tflite::FlatBufferModel> model =
+        tflite::FlatBufferModel::BuildFromFile("../models/face_detection_full_range.tflite");
 
-  TFLITE_MINIMAL_CHECK(model != nullptr);
+    TFLITE_MINIMAL_CHECK(model != nullptr);
 
-  // Build the interpreter
-  tflite::ops::builtin::BuiltinOpResolver resolver;
-  tflite::InterpreterBuilder builder(*model, resolver);
-  std::unique_ptr<tflite::Interpreter> interpreter;
-  builder(&interpreter);
-  TFLITE_MINIMAL_CHECK(interpreter != nullptr);
+    // Build the interpreter
+    tflite::ops::builtin::BuiltinOpResolver resolver;
+    tflite::InterpreterBuilder builder(*model, resolver);
+    std::unique_ptr<tflite::Interpreter> interpreter;
+    builder(&interpreter);
+    TFLITE_MINIMAL_CHECK(interpreter != nullptr);
 
-  interpreter->AllocateTensors();
+    interpreter->AllocateTensors();
 
-  // Get the shape of the first input tensor
-  TfLiteIntArray* input_dims = interpreter->input_tensor(0)->dims;
-  printf("Input shape: ");
-  for (int i = 0; i < input_dims->size; i++) {
-    printf("%d ", input_dims->data[i]);
-  }
-  printf("\n");
-  // Load the image
-  cv::Mat img = cv::imread("../example.jpg", cv::IMREAD_COLOR);
-  cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
-  if (img.empty()) {
-    fprintf(stderr, "Failed to load image\n");
-    exit(1);
-  }
+    // Get the shape of the first input tensor
+    TfLiteIntArray* input_dims = interpreter->input_tensor(0)->dims;
+    printf("Input shape: ");
+    for (int i = 0; i < input_dims->size; i++) {
+        printf("%d ", input_dims->data[i]);
+    }
+    printf("\n");
+    // Load the image
+    cv::Mat img = cv::imread("../example.jpg", cv::IMREAD_COLOR);
+    cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
+    if (img.empty()) {
+        fprintf(stderr, "Failed to load image\n");
+        exit(1);
+    }
+    // Get the input dimensions from the interpreter
+    //int height = interpreter->input_tensor(0)->dims->data[1];
+    //int width = interpreter->input_tensor(0)->dims->data[2];
+    //int channels = interpreter->input_tensor(0)->dims->data[3];
 
-  // Resize the image
-  cv::resize(img, img, cv::Size(interpreter->input_tensor(0)->dims->data[1], interpreter->input_tensor(0)->dims->data[2]));
-  printf("Resized image shape: %d %d\n", img.rows, img.cols);
-  // Copy the image data into the input tensor
-  float* input = interpreter->typed_input_tensor<float>(0);
-  memcpy(input, img.data, img.total() * img.elemSize());
+    // Create a black image with the same dimensions as the model's input
+    //cv::Mat img_dummy(height, width, channels == 1 ? CV_8UC1 : CV_8UC3, cv::Scalar(0));
 
-  // Invoke the model
-  interpreter->Invoke();
+    // Now you can use 'img' as the input to your model
+    // Resize the image
+    cv::resize(img, img, cv::Size(interpreter->input_tensor(0)->dims->data[1], interpreter->input_tensor(0)->dims->data[2]));
+    //printf("Resized image shape: %d %d\n", img_dummy.rows, img_dummy.cols);
+    float min_val = 0; // Set to your desired minimum value
+    float max_val = 1; // Set to your desired maximum value
 
-  // Get the output
-  float* output = interpreter->typed_output_tensor<float>(0);
+    cv::Mat tensor_data;
+    img.convertTo(tensor_data, CV_32FC1, (max_val - min_val) / 255.0, min_val);
+    cv::cvtColor(tensor_data, tensor_data, cv::COLOR_RGB2BGR);
+    cv::imwrite("../resized_image.jpg", tensor_data);
+    cv::cvtColor(tensor_data, tensor_data, cv::COLOR_BGR2RGB);
+    // Copy the image data into the input tensor
+    float* input = interpreter->typed_input_tensor<float>(0);
+    memcpy(input, tensor_data.data, tensor_data.total() * tensor_data.elemSize());
 
-  // Get the shape of the first output tensor
-  TfLiteIntArray* output_dims  = interpreter->output_tensor(0)->dims;
-  printf("Output shape: ");
-  for (int i = 0; i < output_dims ->size; i++) {
-    printf("%d ", output_dims ->data[i]);
-  }
-  printf("\n");
+    // Invoke the model
+    interpreter->Invoke();
 
-  return 0;
+    // Get the output
+    float* output = interpreter->typed_output_tensor<float>(0);
+    // Open a file stream for output
+    std::ofstream file("../output.txt");
+
+    // Check if any output values are NaN and write output to file
+    int output_size = interpreter->output_tensor(0)->bytes / sizeof(float);
+    for (int i = 0; i < output_size; i++) {
+        if (std::isnan(output[i])) {
+            printf("Output contains NaN values.\n");
+            break;
+        }
+        file << output[i] << "\n";  // Write the output value to the file
+    }
+
+    // Don't forget to close the file when you're done
+    file.close();
+
+    // Check if any output values are NaN
+    for (int i = 0; i < output_size; i++) {
+        if (std::isnan(output[i])) {
+            printf("Output contains NaN values.\n");
+            break;
+        }
+    }
+    // Get the shape of the first output tensor
+    TfLiteIntArray* output_dims  = interpreter->output_tensor(0)->dims;
+    printf("Output shape: ");
+    for (int i = 0; i < output_dims ->size; i++) {
+        printf("%d ", output_dims ->data[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
